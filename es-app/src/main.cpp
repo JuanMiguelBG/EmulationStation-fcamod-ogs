e3ca3ab03a33c7b468d2fc0538f99c216c3c8eed
JuanMiguelBG <bacojmbg@gmail.com>
2022-04-06 08:31:26 +0200

    Update song icon
    Update song icon

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -370,7 +370,7 @@ void processAudioTitles(Window* window)
 	{
 		std::string songName = AudioManager::getInstance()->popSongName();
 		if (!songName.empty())
-			window->displayNotificationMessage(_U("\u1F369  ") + songName);
+			window->displayNotificationMessage(_U("\u261F  ") + songName);
 	}
 }
 
