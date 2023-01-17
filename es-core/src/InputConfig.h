3bfac1c67afc80a5c3f49fc53d536e05f0fb53cc
JuanMiguelBG <bacojmbg@gmial.com>
2023-01-17 15:56:11 +0000

    Increment the number of players to 4

--- es-core/src/InputConfig.h
+++ es-core/src/InputConfig.h
@@ -14,7 +14,7 @@ namespace pugi { class xml_node; }
 #define DEVICE_KEYBOARD -1
 #define DEVICE_CEC      -2
 // batocera
-#define MAX_PLAYERS 1
+#define MAX_PLAYERS 4
 
 extern char* BUTTON_OK;
 extern char* BUTTON_BACK;
