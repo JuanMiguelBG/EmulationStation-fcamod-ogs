eb7e80d315236f059e1a151bfca27a44847517b1
JuanMiguelBG <bacojmbg@gmial.com>
2023-02-18 20:11:07 +0000

    Fix OSK Keyboard invocation issue

--- es-app/src/guis/GuiMetaDataEd.cpp
+++ es-app/src/guis/GuiMetaDataEd.cpp
@@ -239,7 +239,7 @@ GuiMetaDataEd::GuiMetaDataEd(Window* window, MetaDataList* md, const std::vector
 					if (Settings::getInstance()->getBool("ShowFileBrowser"))
 						mWindow->pushGui(new GuiFileBrowser(mWindow, dir, filePath, type, updateVal, title));
 					else
-						mWindow->pushGui(new GuiTextEditPopupKeyboard(mWindow, title, filePath, updateVal, false, nullptr));
+						mWindow->pushGui(new GuiTextEditPopupKeyboard(mWindow, title, filePath, updateVal, false));
 				});
 
 				break;
@@ -277,9 +277,9 @@ GuiMetaDataEd::GuiMetaDataEd(Window* window, MetaDataList* md, const std::vector
 				row.makeAcceptInputHandler([this, title, ed, updateVal, multiLine] 
 				{
 					if (!multiLine && Settings::getInstance()->getBool("UseOSK"))
-						mWindow->pushGui(new GuiTextEditPopupKeyboard(mWindow, title, ed->getValue(), updateVal, multiLine, nullptr));
+						mWindow->pushGui(new GuiTextEditPopupKeyboard(mWindow, title, ed->getValue(), updateVal, multiLine));
 					else
-						mWindow->pushGui(new GuiTextEditPopup(mWindow, title, ed->getValue(), updateVal, multiLine, nullptr));
+						mWindow->pushGui(new GuiTextEditPopup(mWindow, title, ed->getValue(), updateVal, multiLine));
 				});
 				break;
 			}
